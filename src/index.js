7b595ca868cabfb888ca6617a460f1c3b2862320
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-07-27 17:29:34 -0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://awesome-node-graphql.herokuapp.com/graphql",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
